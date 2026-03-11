8fb62e56746b1a7167d967b55518e861a64466a5
Drxkaii <116888877+octasoul@users.noreply.github.com>
2026-03-11 09:10:15 +0000

    miatoll: Rewrite libinit
    Co-authored-by: Arian <arian.kulmer@web.de>
    Co-authored-by: basamaryan <basam.aryan@gmail.com>
    Co-authored-by: AdarshGrewal <adarshgrewal@gmail.com>
    Co-authored-by: Cyber Knight <cyberknight755@gmail.com>
    Change-Id: I710c6f951fdd1c552ee7a6823714bf1b57c0b2b0
    Signed-off-by: octasoul <sb3030302@gmail.com>

--- device.mk
+++ device.mk
@@ -7,7 +7,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
@@ -126,7 +126,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
 # Init
-$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_xiaomi_miatoll)
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_miatoll)
 
 # IPACM
 PRODUCT_PACKAGES += \
