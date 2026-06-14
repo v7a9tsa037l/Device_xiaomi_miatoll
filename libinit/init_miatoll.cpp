75d914d3d4e8a2dbe56e0d8a57dad966f56aa76d
octasoul <sb3030302@gmail.com>
2026-06-14 16:19:22 +0000

    fixup! miatoll: Rewrite libinit
    Excalibur was apparently an Indian variant. This fixes
    showing `unknown` device variant in the about section.
    
    Change-Id: Ie43aa7e97a1726a0e2c785d777eccad94161288d

--- libinit/init_miatoll.cpp
+++ libinit/init_miatoll.cpp
@@ -1,28 +1,6 @@
 /*
-   Copyright (C) 2020 The LineageOS Project.
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are
-   met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of The Linux Foundation nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: BSD-3-Clause
  */
 
 #include <vector>
@@ -109,6 +87,11 @@ void vendor_load_properties() {
             model = "POCO M2 Pro";
             device = "gram";
             fingerprint = "POCO/gram_in/gram:12/RKQ1.211019.001/V14.0.5.0.SJPINXM:user/release-keys";
+        } else if (hwname == "excalibur") {
+            brand = "Redmi";
+            model = "Redmi Note 9 Pro Max";
+            device = "excalibur";
+            fingerprint = "Redmi/excalibur_in/excalibur:12/RKQ1.211019.001/V14.0.2.0.SJXINXM:user/release-keys";
         }
     } else {
         if (hwname == "curtana") {
@@ -121,11 +104,6 @@ void vendor_load_properties() {
             model = "Redmi Note 9 Pro";
             device = "joyeuse";
             fingerprint = "Redmi/joyeuse_global/joyeuse:12/RKQ1.211019.001/V14.0.3.0.SJZMIXM:user/release-keys";
-        } else if (hwname == "excalibur") {
-            brand = "Redmi";
-            model = "Redmi Note 9 Pro Max";
-            device = "excalibur";
-            fingerprint = "Redmi/excalibur_in/excalibur:12/RKQ1.211019.001/V14.0.2.0.SJXINXM:user/release-keys";
         }
     }
 
