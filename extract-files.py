eca66ef86c6c07c78d1cdcb2439dfc31a7638065
RiteshSahany <ritesh@thecloverproject.com>
2026-07-09 11:40:31 +0000

    miatoll: Update Media stack from V816.0.6.0.TKXMIXM
    Change-Id: I34b629dabd529533d267a9b5b1ff4046e07d69bf
    Signed-off-by: iHSAN <ihsanulrahman@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -21,7 +21,7 @@ namespace_imports = [
     'device/xiaomi/miatoll',
     'hardware/qcom-caf/sm8150',
     'hardware/qcom-caf/wlan',
-    'vendor/hardware/xiaomi',
+    'hardware/xiaomi',
     'vendor/qcom/opensource/commonsys-intf/display',
     'vendor/qcom/opensource/commonsys/display',
     'vendor/qcom/opensource/dataservices',
