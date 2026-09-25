4d95d3b0781de72254a25dffc789ca9b7798f5b0
v7a9tsa037l <v7a9tsa037l@gmail.com>
2026-09-25 17:18:20 +0000

    miatoll: overlays entry fixup

--- device.mk
+++ device.mk
@@ -189,7 +189,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     SM6250Aperture \
-  SM6250LineageDialer \
+    SM6250LineageDialer \
     SM6250LineageSDK \
     SM6250LineageSettingsOverlay
 
